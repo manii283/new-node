a33b7708e91a227e7f31f839b40704a10b649f05
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-30 20:15:03 +0530

    node js routing

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,7 +1,21 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    res.end('hello from other sides');
+    // console.log(req.url)
+    if(req.url == '/'){
+        res.end('hello from other sides');
+    }
+    else if(req.url == '/about'){
+        res.end('hello from about');
+    }
+    else if(req.url == '/contact'){
+        res.end('hello from contact');
+    }
+    else{
+        res.writeHead(404, {"Content-type" : "text/html"})
+        res.end('<h1>404 error page</h1>');
+    }
 });
 server.listen(8000, () => {
     console.log('listing to the port no 8000')
